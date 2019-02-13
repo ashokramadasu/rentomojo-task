cc8e7ed1e63e2d2563f4ff6f2d4829e779e9bc34
ashok_sriramadasu <ashok@unitary.io>
2019-02-13 10:49:47 +0530

    model changes added

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    port : 3000,
+    MongoURI: 'mongodb+srv://root:root@cluster0-aj3tj.mongodb.net/test?retryWrites=true',
+    DBname: 'scraping'
+}
--- server.js
+++ server.js
@@ -1,19 +1,39 @@
 const express = require('express')
     ,request = require('request')
+    ,throttledRequest = require('throttled-request')(request)
     ,cheerio = require('cheerio')
     ,app = express();
+ 
+throttledRequest.configure({
+    requests: 5,
+    milliseconds: 1000
+});//This will throttle the requests so no more than 5 are made every second
 
 app.get('/getLinks', function (req, res) {
     let url = 'https://medium.com/';
 
     request(url, function (error, response, html) {
-        if (!error) {
-            let $ = cheerio.load(html.toString());
+        if (!error) {            let $ = cheerio.load(html.toString());
+
             let result = []
             $("a").each((i, link) => {
-                result.push($(link).attr("href"))
+                // result.push($(link).attr("href"))
+                let scrapedUrl = $(link).attr("href").split('?');
+                console.log(scrapedUrl)
+                let urlPart = scrapedUrl[0];
+                let Params = (scrapedUrl[1]) ? scrapedUrl[1].split('&') : [];
+                console.log('Params',Params)
+                let paramsPart = [...new Set(Params)];
+                result.push(urlPart)
+
+
             });
-            res.send(result);
+
+            let urls = new Map([...new Set(result)].map(
+                x => [x, result.filter(y => y === x).length]
+            ));
+            console.log(urls);
+            res.send(urls);
         };
     });
 })
