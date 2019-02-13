3da3dd7923ac9cf8d57468ed8e6dcbfe4251f397
Ashok <asriramadasu@instarem.com>
2019-02-14 00:37:47 +0530

    unique urls and its count and params  added in data JSON

---
+++ model.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+ 
+const urlSchema = new Schema({
+  url: { type: String, unique: true},
+  reference_count: Number,
+  params: String
+});
--- server.js
+++ server.js
@@ -1,42 +1,51 @@
-const express = require('express')
-    ,request = require('request')
-    ,throttledRequest = require('throttled-request')(request)
-    ,cheerio = require('cheerio')
-    ,app = express();
- 
-throttledRequest.configure({
-    requests: 5,
-    milliseconds: 1000
-});//This will throttle the requests so no more than 5 are made every second
+const express     = require('express')
+    , request     = require('request')
+    , rp          = require('request-promise')
+    , cheerio     = require('cheerio')
+    , mongoose    = require('mongoose')
+    , config      = require('./config')
+    , URL         = require('url')
+    , app         = express();
 
-app.get('/getLinks', function (req, res) {
-    let url = 'https://medium.com/';
-
-    request(url, function (error, response, html) {
-        if (!error) {            let $ = cheerio.load(html.toString());
-
-            let result = []
-            $("a").each((i, link) => {
-                // result.push($(link).attr("href"))
-                let scrapedUrl = $(link).attr("href").split('?');
-                console.log(scrapedUrl)
-                let urlPart = scrapedUrl[0];
-                let Params = (scrapedUrl[1]) ? scrapedUrl[1].split('&') : [];
-                console.log('Params',Params)
-                let paramsPart = [...new Set(Params)];
-                result.push(urlPart)
+     mongoose.createConnection(config.MongoURI, { useNewUrlParser: true })
+     .then(conn => {
+        console.log('conn', conn);
+     })
+     .catch(err => {
+         console.log('err', err);
+     })
 
 
-            });
+let getLinks = async function (req, res) {
+    let uri = 'https://medium.com/';
+    let html = await rp.get(uri);
 
-            let urls = new Map([...new Set(result)].map(
-                x => [x, result.filter(y => y === x).length]
-            ));
-            console.log(urls);
-            res.send(urls);
-        };
+    let $ = cheerio.load(html.toString());
+    let result = []
+
+    $("a").each((i, link) => {
+        let u = URL.parse($(link).attr("href"), true);
+        let params = Object.keys(u.query);
+        let url = u.href.split('?')[0];
+        result.push({ url, params});
     });
-})
+
+    let data = await ([...new Set(result.map(x => x.url))].map(
+        x => { return {
+            url: x, 
+            reference_count: result.filter(y => y.url === x).length, 
+            params: result.find(z => z.url === x).params,
+           } 
+        }
+    ));
+
+    res.send(data);
+
+};
+
+
+
+app.get('/getLinks', getLinks);
 
 app.listen('3000')
 console.log('Express Server listening on port 3000');
